af0588c90f6b4b2e13a4f46cb8ec65038393f5e8
Danish <34064567+danishnizmi@users.noreply.github.com>
2025-04-23 10:55:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -402,14 +402,18 @@ except Exception as e:
             </div>
             
             <script>
-                function clearCookiesAndReload() {
-                    document.cookie.split(";").forEach(function(c) {
-                        document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-                    });
+                function clearCookiesAndReload() {{
+                    var cookies = document.cookie.split(";");
+                    for (var i = 0; i < cookies.length; i++) {{
+                        var cookie = cookies[i];
+                        var eqPos = cookie.indexOf("=");
+                        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+                        document.cookie = name + "=;expires=" + new Date().toUTCString() + ";path=/";
+                    }}
                     localStorage.clear();
                     sessionStorage.clear();
                     location.reload();
-                }
+                }}
             </script>
         </body>
         </html>
